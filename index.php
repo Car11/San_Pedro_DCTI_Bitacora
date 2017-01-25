6e57764b46cbf612e0aeda239034c0110de2e81d
Car11 <CaChac6@icetel.ice>
2017-01-25 14:20:55 -0600

    inicia cambios en estructura index

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
     }
 	/*print $type."<br>";
 	print $cedula."<br>";
-	print $detalle."<br>";*/
+	print $detalle."<br>";
+	exit;*/
 ?>
 <html>
 <head>
@@ -33,54 +34,30 @@
         <div class="contenido" >
             <div id="error">Formato de cedula: 9 digitos sin guiones ni espacios</div>
             <h2>Número de cédula</h2>
-            <form  action="EnviaVisitante.php" method="POST">
-                <input type="text" maxlength="9" id="cedula" class="input-field" name="cedula" placeholder="0 0000 0000" title="Número de cédula separado con CEROS"   onkeypress="return isNumber(event)"/>
-                <div class="detalle">
-                    <h3>Detalle de la Visita</h3>
-                    <textarea type="text" class="textarea-field"  id = "detalle" name="detalle" placeholder="Descripción  /  #RFC" ></textarea>
-                </div>
-                <div>
-                    <input type="submit" value="Enviar" id="enviar" />
-                </div>
-            </form>
+            <div id="form">
+                <!--<form  action="EnviaVisitante.php" method="POST">  
+                    <div id="divcedula">     
+                        <input type="text" maxlength="9" id="cedula" class="input-field" name="cedula" placeholder="0 0000 0000" title="Número de cédula separado con CEROS"   onkeypress="return isNumber(event)"/>
+                    </div>	            	
+                    <div id="divdetalle">
+                        <h3>Detalle de la Visita</h3>
+                        <textarea type="text" class="textarea-field"  id = "detalle" name="detalle" placeholder="Descripción  /  #RFC" ></textarea>	                
+
+                        <input type="submit" value="Enviar" id="enviar" />
+                    </div>
+                </form> -->   
+            </div>
+            <div id="mensajes">
+                <!--<div id="checkingreso"><img src="img/Check.png" height="50"  alt="logo"></div>
+                <div id="salidaDetalle">
+                    <img src="img/detalle.png" height="50"  alt="logo"  style="margin:0px"/> 
+                </div>    -->
+            </div>
         </div>
-         <div id="ingreso"><img src="img/Check.png" height="50"  alt="logo" /> </div>
-        <div id="salidaDetalle"><img src="img/detalle.png" height="50"  alt="logo" /> </div>
     </article>
 <script>
-    var cedula = '<?php print $cedula ?>';
-    var type = '<?php print $type ?>';
-    $("#cedula").focus();
-    //
-    if(type == "NULL")
-    {
-    	//Oculta los iconos de mensajes
-        $( "#ingreso" ).hide();
-        $("#salidaDetalle").hide();
-    }
-    else if(type == "IN")
-    {    	
-    	$("#salidaDetalle").hide();
-    	//mensaje bienvenida
-        $( "#ingreso" ).fadeIn("slow", function(){
-             $( "#ingreso" ).fadeOut(4000);
-        });        
-    }
-    else if(type == "OUT")
-    {
-    	$( "#ingreso" ).hide();
-        $("#salidaDetalle").show(); // MENSAJE editar detalle Y DAR OTRO CLIC ENVIAR
-        var f = document.getElementById('salidaDetalle');
-        setInterval(function() {
-            f.style.display = (f.style.display == 'none' ? '' : 'none');
-        }, 850); 
-        //
-        document.getElementById("detalle").value= '<?php  print $detalle ?>';
-        document.getElementById("cedula").value=cedula;
-        //document.getElementById("cedula").readOnly = true;
-        $("#detalle").fadeIn(1000);
-        $("#detalle").focus();
-    }
+ 	
+  
 </script>
 </body>
 </html>
