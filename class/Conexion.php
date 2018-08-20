bb7ae8a81b7873527d4b1fbc2555299b53a68f24
Car11 <carlos.echc11@gmail.com>
2018-08-20 00:38:24 -0600

    4nivel

--- class/Conexion.php
+++ class/Conexion.php
@@ -8,14 +8,8 @@ class DATA {
 
 	private static function ConfiguracionIni(){
         require_once('Globals.php');
-        if (file_exists('../../ini/config.ini')) {
-            self::$config = parse_ini_file('../../ini/config.ini',true); 
-        } 
-        else if (file_exists('../ini/config.ini')) {
-            self::$config = parse_ini_file('../ini/config.ini',true); 
-        }         
-        else if (file_exists('../../../ini/config.ini')) {
-            self::$config = parse_ini_file('../../../ini/config.ini',true); 
+        if (file_exists('../../../../ini/config.ini')) {
+            self::$config = parse_ini_file('../../../../ini/config.ini',true); 
         }         
     }  
 
